bb300b4d7bf7da2b4714508cc21a7098661865e5
matias-saig <saig.matias@gmail.com>
2024-05-04 23:43:52 -0300

    Desafio_5

--- Desafio_5.py
+++ Desafio_5.py
@@ -11,10 +11,58 @@
     calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que
     fuera divisible por 400.
     Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era
-    el calendario vigente en ese año:2
-    Ingrese el año a evaluar: 2011
-    El año 2011 NO es bisiesto
-    Ingrese el año a evaluar: 2024
-    El año 2024 es bisiesto
+    el calendario vigente en ese año
 """
 
+
+# función de control
+def query_ctrl(msg):
+    while True:
+        try:
+            time = int(input(msg))
+
+            if (time != 0) and (time > -45):
+                return time
+            elif time == 0:
+                print('En el calendario juliano no hay año 0.\n')
+            elif time < -45:
+                print(
+                    'Los bisiestos se cuentan a partir del año -45 A.C., no se incluyen los anteriores\n')
+
+        except ValueError:
+            print('El valor no es válido. Ingrese el año a evaluar (ejemplo 150 o -44): ')
+
+
+# Inicio
+print("""
+      Años bisiestos
+      Calendario juliano: -45 a 1581; Calendario gregoriano: 1582 en adelante
+      (Ingresar 9999 para finalizar)""")
+
+
+while True:
+    cal_greg = "calendario gregoriano -"
+    cal_jul = "calendario juliano -"
+
+    # Pregunta
+    year = query_ctrl("\nIngrese el año a evaluar: ")
+
+    # Finalización
+    if year == 9999:
+        break
+
+    # Evaluación según calendario juliano
+    if year >= -45 and year <= 1581:
+        if year % 4 == 0:
+            print(f"{cal_jul} {year} es un año bisiesto")
+        else:
+            print(f"{cal_jul} {year} NO es un año bisiesto")
+
+    # Evaluación según calendario gregoriano
+    elif year >= 1582:
+        if year % 4 == 0 and not year % 100 == 0:
+            print(f"{cal_greg} {year} es un año bisiesto")
+        elif year % 4 == 0 and year % 400 == 0:
+            print(f"{cal_greg} {year} es un año bisiesto")
+        else:
+            print(f"{year} NO es un año bisiesto {cal_greg}")
