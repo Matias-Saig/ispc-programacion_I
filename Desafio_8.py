d4c6c204d1f4cc0cbe697debd76a565a9e3644fa
matias-saig <saig.matias@gmail.com>
2024-05-12 01:37:48 -0300

    Desafio_8

--- Desafio_8.py
+++ Desafio_8.py
@@ -7,30 +7,85 @@ Escriba un programa que pida al usuario elegir el producto y luego le pida ingre
 monedas hasta alcanzar el monto a pagar. Si el monto ingresado es mayor que el precio
 del producto, el programa debe entregar las monedas de vuelto, una por una.
 
-Producto A 270
-Producto B 340
-Producto C 390
-
-Monedas
-    10
-    50
-    100
-
-Selección de producto
-ingresar dinero
-vuelto
-
 """
 
-# control
-def query_ctrl(msg, error):
+# control ingreso producto
+def query_products(msg, products):
+    while True:
+        control = input(msg).upper()    
+        if control in products:
+            return products[control]
+        else:
+            print("El producto ingresado no es correcto. Seleccione el producto A, B o C\n")
+
+
+# control ingreso dinero
+def query_ctrl_number(msg, error, coins):
     while True:
         try:
             control = int(input(msg))
-            if control >= 0:
+            if control in coins:
                 return control
             else:
-                print("El número debe ser positivo")
+                print(error, "\n")
 
         except ValueError:
-            print(error)
+            print(error, "\n")
+
+
+# Ingresar dinero
+def coin_add(n, product, coins):
+    if n > 0:
+        print(f"  Dinero ingresado: ${n}   Faltante: ${product - n}\n")
+
+    add = query_ctrl_number("Cuánto quieres ingresar?: ",
+                            "Solo puedes ingresar 10, 50 0 100", coins)
+    
+    for coin in coins:
+        if coin == add:
+            n += coin
+    
+    return n
+
+# Vuelto de dinero
+def money_back(n, product, coins):
+    rest = n - product
+    change = 0
+    while rest > 0:
+        for coin in coins:
+            if rest >= coin:
+                rest -= coin
+                change += coin
+                print(f"  ${coin}")
+    return change
+
+
+# Cálculo total
+def total_amount(product, coins):
+    print(f"El total a pagar es ${product} \n")
+    print(f"Puedes ingresar {coins[2]}, {coins[1]} y {coins[0]} para pagar")
+
+    money = 0
+    while money < product:
+        money = coin_add(money, product, coins)
+    if money == product:
+        print(
+        f"Producto: ${product}    Total ingresado: ${money} \n- Producto pagado - \n")
+
+    if money > product:
+        print("\nSu vuelto:")
+        change = money_back(money, product, coins)
+        print(f"\nProducto: ${product}    Total ingresado: ${money}    Vuelto: ${change} \n- Producto pagado - \n")
+
+# Ejecución
+products = {"A": 270, "B": 340, "C": 390}
+
+print("Productos disponibles:")
+for opc, cost in products.items():
+        print(f"    {opc}) ${cost}")
+
+coins = (100, 50, 10)
+
+selected = query_products(f"Seleccione un producto:", products)
+
+total_amount(selected, coins)
